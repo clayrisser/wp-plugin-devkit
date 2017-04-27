6cf8cf918c2e69b5bbc505e217b7505734de7530
Jam Risser <jam@jamrizzi.com>
2017-04-27 14:43:52 -0500

    Created base php script

--- Makefile
+++ Makefile
@@ -2,21 +2,23 @@ CWD := $(shell readlink -en $(dir $(word $(words $(MAKEFILE_LIST)),$(MAKEFILE_LI
 PLUGIN_NAME := plugin-name
 
 .PHONY: all
-all: fetch_dependancies
+all: clean fetch_dependancies
 
 .PHONY: start
 start:
-	docker run --name some-mariadb --rm -d -e MYSQL_ROOT_PASSWORD=hellodocker mariadb:latest
 	docker run --name some-instant-wordpress --rm --link some-mariadb:mysql -p 8080:80 -v $(CWD)/$(PLUGIN_NAME):/var/www/html/wp-content/plugins/$(PLUGIN_NAME) jamrizzi/instant-wordpress:latest
+
+.PHONY: database
+database:
+	docker run --name some-mariadb --rm -d -e MYSQL_ROOT_PASSWORD=hellodocker mariadb:latest
 
 .PHONY: init
 init:
 	php init.php
 
-.PHONY: kill
-kill:
-	docker stop some-mariadb some-wordpress
-	docker rm some-mariadb some-wordpress
+.PHONY: clean
+clean:
+	$(info cleaned)
 
 .PHONY: fetch_dependancies
 fetch_dependancies: docker
---
+++ tools.php
@@ -0,0 +1,65 @@
+<?php
+
+function main($argv) {
+  $command = $argv[count($argv) - 1];
+  switch($command) {
+  case "init":
+    init();
+    break;
+  default:
+    write_ln("Invalid command", 1);
+  }
+}
+
+function init() {
+  $name = read_ln("Plugin Name: ");
+  $author = read_ln("Plugin Author: ");
+  write_ln("The plugin is called ".$name);
+  write_ln("Your name is ".$author);
+  $files = recursively_get_files('./');
+  foreach($files as $path) {
+    find_and_replace($path, 'plugin-name', $name)
+  }
+}
+
+function recursively_get_files($path) {
+  $directory_iterator = new RecursiveDirectoryIterator($path);
+  $iterator_iterator  = new RecursiveIteratorIterator($directory_iterator, RecursiveIteratorIterator::SELF_FIRST);
+  $files = array();
+  foreach ($iterator_iterator as $file) {
+    $path = $file->getRealPath();
+    if ($file->isFile()) {
+      array_push($files, $path);
+    }
+  }
+  return $files;
+}
+
+function find_and_replace($path, $find, $replace) {
+  $file = file_get_contents($path);
+  $file = str_replace($find, $replace, $contents);
+  file_put_contents($path, $file);
+}
+
+function write($message, $code = 0) {
+  switch($code) {
+  case 0:
+    fwrite(STDOUT, $message);
+    return $code;
+  default:
+    fwrite(STDERR, $message);
+    return exit($code);
+  }
+}
+
+function write_ln($message, $code = 0) {
+  return write($message."\n", $code);
+}
+
+function read_ln($message) {
+  if ($message) write($message);
+  $stdin = fgets(STDIN);
+  return substr($stdin, 0, strlen($stdin) - 1);
+}
+
+main($argv);
